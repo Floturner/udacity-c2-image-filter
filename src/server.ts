e63d639e48f947158c309638c7624d856314ebca
Florent Taboukouna <56768827+Floturner@users.noreply.github.com>
2023-01-08 23:33:56 +0000

    deployed app to elastic beanstalk

--- src/server.ts
+++ src/server.ts
@@ -13,21 +13,6 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 	app.use(bodyParser.json());
 
 	// @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-	// GET /filteredimage?image_url={{URL}}
-	// endpoint to filter an image from a public url.
-	// IT SHOULD
-	//    1
-	//    1. validate the image_url query
-	//    2. call filterImageFromURL(image_url) to filter the image
-	//    3. send the resulting file in the response
-	//    4. deletes any files on the server on finish of the response
-	// QUERY PARAMATERS
-	//    image_url: URL of a publicly accessible image
-	// RETURNS
-	//   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-	/**************************************************************************** */
-
 	app.get(
 		"/filteredimage",
 		async (req: Request, res: Response, next: NextFunction) => {
@@ -62,8 +47,6 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
 			}
 		}
 	);
-
-	//! END @TODO1
 
 	// Root Endpoint
 	// Displays a simple message to the user
